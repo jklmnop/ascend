fb83ce68557d1eda93ee515f9ce08f6de9f2c7bb
jason kühn <spaceyraygun@gmail.com>
2013-10-14 15:40:48 -0400

    refactor footer

--- sites/all/themes/custom/ascend_omega/templates/html.tpl.php
+++ sites/all/themes/custom/ascend_omega/templates/html.tpl.php
@@ -6,15 +6,6 @@
 <head<?php print $rdf->profile; ?>>
   <?php print $head; ?> 
     <?php $theme_path = drupal_get_path('theme', variable_get('theme_default', NULL)); ?>
-    <link rel="shortcut icon" href="<?= $theme_path; ?>/favicon.ico" type="image/vnd.microsoft.icon" />
-    <!-- For third-generation iPad with high-resolution Retina display: -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?= $theme_path; ?>/apple-touch-icon-144x144-precomposed.png">
-    <!-- For iPhone with high-resolution Retina display: -->
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?= $theme_path; ?>/apple-touch-icon-114x114-precomposed.png">
-    <!-- For first- and second-generation iPad: -->
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?= $theme_path; ?>/apple-touch-icon-72x72-precomposed.png">
-    <!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
-    <link rel="apple-touch-icon-precomposed" href="<?= $theme_path; ?>/apple-touch-icon-precomposed.png">
   
   <title><?php print $head_title; ?></title>  
   <?php print $styles; ?>
@@ -28,10 +19,7 @@
     
   <?php print $page_top; ?>
   <?php print $page; ?>
-    <div class="fine-print container-12 clearfix">
-        <div class="copyright grid-6"><small><?php print(date('Y')); ?> &copy; Ascend Psychological Services, LLC.  All Rights Reserved.</small></div>
-        <div class="site-by grid-6"><small>Site design by <a href="http://parachuteagency.com/" rel="nofollow" title="Parachute Agency">Parachute Agency</a></small></div>
-    </div>
+    
   <?php print $page_bottom; ?>
 </body>
 </html>
